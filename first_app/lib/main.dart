424db3e975e9bd1377a4e53d777891d26bc292c0
nabalmes <neilbalmes4200@gmail.com>
2024-04-15 16:51:11 +0800

    basic styling

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
-  const hello = 'Hello Bitchesss!';
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(206, 155, 39, 176),
-                Color.fromARGB(159, 157, 64, 173),
-                Color.fromARGB(141, 196, 87, 215),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              hello,
-              style: TextStyle(
-                color: Color.fromARGB(255, 36, 28, 28),
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
